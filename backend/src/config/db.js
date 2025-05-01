c691a8258a2cefb6a72c4288970fb41a7cecbb51
Hector <hprecva@gmail.com>
2025-04-30 23:01:37 -0600

    Contenedores Docker

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,6 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const mongoose = require('mongoose');
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/miapp';
 
-module.exports = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ error: 'Acceso denegado' });
-    
-    try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verified;
-        next();
-    } catch (error) {
-        res.status(400).json({ error: 'Token no válido' });
-    }
-};
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((err) => console.error("Error conectando a MongoDB", err));
